a707d781d911ec8042283cea6c8286dd22de52c1
Omri-Levy <omri.levy0197@gmail.com>
2020-12-29 06:44:51 +0200

    refactor: now combining instead of overriding the pageContainer class to allow DRY code

--- src/components/pages/Portfolio/useStyles.ts
+++ src/components/pages/Portfolio/useStyles.ts
@@ -1,11 +1,16 @@
 import { makeStyles } from '@material-ui/core/styles';
+import getCustomBreakpoints from '~/utils/getCustomBreakpoints';
 
 const useStyles = makeStyles((theme) => ({
 	portfolioContainer: {
 		height: `unset`,
-		width: `100%`,
 		[`@media (max-width: 1280px) and (min-width: 1000px)`]: {
 			marginTop: 59,
+		},
+		[getCustomBreakpoints(`md`, theme, `down`)]: {
+			display: `block`,
+			flexDirection: `unset`,
+			alignItems: `unset`,
 		},
 	},
 }));
