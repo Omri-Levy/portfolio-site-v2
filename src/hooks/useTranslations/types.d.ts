0239c60113b4806c7fdc80f01abbc212c3965b78
Omri-Levy <omri.levy0197@gmail.com>
2021-01-02 18:11:03 +0200

    test: added a test for useTranslations.

--- src/hooks/useTranslations/types.d.ts
+++ src/hooks/useTranslations/types.d.ts
@@ -2,5 +2,8 @@ type He = string;
 type En = string;
 
 type Translation = (he: He, en: En) => He | En;
+interface Props {
+	rtl: boolean;
+}
 
-export { Translation };
+export { Translation, Props };
--- src/hooks/useTranslations/useTranslations.test.tsx
+++ src/hooks/useTranslations/useTranslations.test.tsx
@@ -1,5 +1,21 @@
+import React from 'react';
+import { renderWithProviders, screen } from '~/utils/testUtils';
+import MockComponent from './MockComponent';
+
 describe(`useTranslations`, () => {
-	it(`useTranslations temp`, () => {
-		expect(true).toBeTruthy();
+	it(`returns english`, () => {
+		renderWithProviders(<MockComponent rtl={false} />);
+
+		expect(
+			screen.getByRole(`heading`, { name: /english/i }),
+		).toBeInTheDocument();
+		expect(screen.queryByText(/עברית/i)).not.toBeInTheDocument();
+	});
+
+	it(`returns hebrew`, () => {
+		renderWithProviders(<MockComponent rtl={true} />);
+
+		expect(screen.getByRole(`heading`, { name: /עברית/i })).toBeInTheDocument();
+		expect(screen.queryByText(/english/i)).not.toBeInTheDocument();
 	});
 });
