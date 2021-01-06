ebf3d99bc47fc33cdfe9e336dae0f5250b05b75a
Omri-Levy <omri.levy0197@gmail.com>
2021-01-06 12:18:33 +0200

    refactor: now the logo is the same color as the links for contrast, now the active link has an underline instead of an active color to avoid contrast issues.

--- cypress/e2e/smoke.ts
+++ cypress/e2e/smoke.ts
@@ -1,6 +1,7 @@
 describe(`app`, () => {
 	it(`works`, () => {
 		cy.visit(`/`);
+		cy.wait(0.1);
 		cy.findByRole(`link`, { name: /dive in/i }).click();
 	});
 });
--- src/components/Layout/Header/Nav/NavLink/useStyles.ts
+++ src/components/Layout/Header/Nav/NavLink/useStyles.ts
@@ -1,36 +1,49 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const linkProps = {
-	fontSize: 13,
-	fontWeight: 400,
-	textDecoration: `none`,
-	'&:hover': {
-		textDecoration: `none`,
-	},
-	marginRight: 23,
-	'&:last-child': {
-		marginRight: 0,
-	},
-};
-
 const useStyles = makeStyles((theme) => ({
 	listItem: {
-		display: `flex`,
-		flexDirection: `column`,
+		minWidth: 80,
 	},
 	icon: {
-		color: theme.palette.secondary.light,
-	},
-	activeIcon: {
-		color: theme.palette.secondary.dark,
+		color: theme.palette.text.primary,
 	},
 	link: {
-		...linkProps,
-		color: theme.palette.secondary.light,
+		display: `flex`,
+		flexDirection: `column`,
+		alignItems: `center`,
+		color: theme.palette.text.primary,
+		fontSize: 13,
+		fontWeight: 400,
+		textDecoration: `none`,
+		'&:hover': {
+			textDecoration: `none`,
+		},
+		marginRight: 23,
+		'&:last-child': {
+			marginRight: 0,
+		},
+		'&::after': {
+			// eslint-disable-next-line
+			content: '""',
+			width: 0,
+			transition: `width 0.3s ease-in-out`,
+			height: 3,
+			background: theme.palette.text.primary,
+		},
+		'&:hover::after': {
+			width: `100%`,
+			transition: `width 0.3s ease-in-out`,
+		},
 	},
 	activeLink: {
-		...linkProps,
-		color: theme.palette.secondary.dark,
+		'&::after': {
+			// eslint-disable-next-line
+			content: '""',
+			width: `100%`,
+			transition: `width 0.3s ease-in-out`,
+			height: 3,
+			background: theme.palette.text.primary,
+		},
 	},
 }));
 
